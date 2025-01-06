de8e2d308132b8ef818232c07245136361ddda22
Himmu9131 <114733178+Himmu9131@users.noreply.github.com>
2025-01-06 19:34:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+	let trimed=s.trim();
+	let ind=trimed.indexOf(' ');
+	if(ind==-1) return trimed;
+	return trimed.substring(0,ind+1);
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s)); 
