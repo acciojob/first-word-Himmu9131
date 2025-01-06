0f8462bd8831ed418e65528245cd650322303868
Himmu9131 <114733178+Himmu9131@users.noreply.github.com>
2025-01-06 19:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function firstWord(s) {
   // your code here
 	let trimed=s.trim();
-	let ind=trimed.indexOf(' ');
+	let ind=trimed.indexOf(" ");
 	if(ind==-1) return trimed;
-	return trimed.substring(0,ind+1);
+	return trimed.substring(0,ind);
 }
 
 // Do not change the code below
